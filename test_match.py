571ff23179c7295303a90a657b3419c492d185eb
Justin Pearson <justinpearson@users.noreply.github.com>
2021-07-08 08:48:30 -0700

    Remove pytest fixtures for test clarity.

--- test_match.py
+++ test_match.py
@@ -7,97 +7,78 @@ class DummyArgs:
 	title_search_strs : list = field(default_factory=list)
 	body_search_strs : list = field(default_factory=list)
 	match_ANY_title : bool = False
-	match_ANY_body : bool = False 
+	match_ANY_body : bool = False
 	match_title_OR_body : bool = False
 
-from pytest import fixture
-from io import StringIO
-from textwrap import dedent
-from lib import parse_notes_file_into_entries
-
-@fixture
-def entries():
-	return parse_notes_file_into_entries( StringIO( dedent( '''\
-		2020-01-01
-		List Of Apples
-
-		- Red delicious: too mealy
-		- Gravenstein: can't remember
-		- Granny smith: too tart
-		- Gala: too small
-		- Honeycrisp: too expensive
-
-
-		2020-02-02
-		List of chapter intros of Pride and Prejudice
-
-		Chapter 1 It is a truth universally acknowledged,
-		that a single man in possession of a good fortune, 
-		must be in want of a wife. 
-
-		Chapter 2 ...
-		''')))
-
-@fixture
-def entry_apples(entries):
-	return entries[0]
-
-@fixture
-def entry_book(entries):
-	return entries[1]	
-
-
-def test_search_is_case_insensitive(entry_apples):
+def test_search_is_case_insensitive():
+	e = { 'title': 'List Of Apples', 'body': ['Red','Green','...'] }
 	a = DummyArgs( title_search_strs = ['apple'] )
-	b = DummyArgs( title_search_strs = ['zapple'] )
-	assert entry_matchesQ(entry_apples, a)
-	assert not entry_matchesQ(entry_apples, b)
+	assert entry_matchesQ(e, a)
 
+def test_search_for_str_in_title_not_the_other_way_around():
+	e = { 'title': 'List Of Apples', 'body': ['Red','Green','...'] }
+	a = DummyArgs( title_search_strs = ['zapple'] )
+	assert not entry_matchesQ(e, a)
 
-def test_regex(entry_apples, entry_book):
-	a = DummyArgs( title_search_strs = [r'\bof\b'] )
-	assert entry_matchesQ(entry_apples, a)
-	assert entry_matchesQ(entry_book, a)
+def test_regex():
+	es_match = [ { 'title': 'How MPEG compression works', 'body': [] }
+			   , { 'title': 'My cars MPG sucks', 'body': [] }
+			   , { 'title': 'File.mpg', 'body': [] }
+			   , { 'title': 'File.mpeg', 'body': [] }
+			   ]
+	es_no_match = [ { 'title': 'Cat', 'body': [] }
+				  , { 'title': 'Dog', 'body': [] }
+				  ]
+	a = DummyArgs( title_search_strs = [r'mpe?g'] )
+	for e in es_match:
+		assert entry_matchesQ(e, a)
+	for e in es_no_match:
+		assert not entry_matchesQ(e, a)
 
+def test_match_ALL_title_strings():
+	e1 = { 'title': 'List of apples', 'body': [] }
+	e2 = { 'title': 'List of poems', 'body': [] }
 
-def test_title_strings(entry_apples, entry_book):
-	a = DummyArgs( title_search_strs = ['apple', 'list of'] )
-	assert entry_matchesQ(entry_apples, a)
-	assert not entry_matchesQ(entry_book, a)
+	a = DummyArgs( title_search_strs = ['list'] )
+	assert entry_matchesQ(e1, a)
+	assert entry_matchesQ(e2, a)
 
-	a = DummyArgs( title_search_strs = ['list of'] )
-	assert entry_matchesQ(entry_apples, a)
-	assert entry_matchesQ(entry_book, a)
+	a = DummyArgs( title_search_strs = ['apple', 'list'] )
+	assert entry_matchesQ(e1, a)
+	assert not entry_matchesQ(e2, a)
 
+def test_match_ANY_title_string():
+	e1 = { 'title': 'List of apples', 'body': [] }
+	e2 = { 'title': 'List of poems', 'body': [] }
 
-def test_match_ANY_title(entry_apples, entry_book):
-	a = DummyArgs( title_search_strs = ['apple', 'list of']
+	a = DummyArgs( title_search_strs = ['apple', 'list']
 				 , match_ANY_title = True
 				 )
-	assert entry_matchesQ(entry_apples, a)
-	assert entry_matchesQ(entry_book, a)
+	assert entry_matchesQ(e1, a)
+	assert entry_matchesQ(e2, a)
 
+def test_match_ALL_body_strings():
+	e1 = {'title': '', 'body': ['aaa bbb', 'ccc']}
+	e2 = {'title': '', 'body': ['aaa','bbb']}
+	a = DummyArgs( body_search_strs = ['aaa','bbb','ccc'] )
+	assert entry_matchesQ(e1, a)
+	assert not entry_matchesQ(e2, a)
 
-def test_body_strings(entry_apples, entry_book):
-	a = DummyArgs( body_search_strs = ['truth','universally','acknowledged'] )
-	assert not entry_matchesQ(entry_apples, a)
-	assert entry_matchesQ(entry_book, a)
+def test_match_ANY_body_string():
+	e = {'title': '', 'body': ['aaa','bbb']}
 
+	a = DummyArgs( body_search_strs = ['aaa','bbb','ccc'] )
+	assert not entry_matchesQ(e, a)
 
-def test_match_ANY_body(entry_apples, entry_book):
-	s = ['falsehood','universally','acknowledged']
+	a.match_ANY_body = True
+	assert entry_matchesQ(e, a)
 
-	a = DummyArgs( body_search_strs = s )
-	assert not entry_matchesQ( entry_book, a )
+def test_match_title_OR_body():
+	e = {'title': 'aaa', 'body': ['bbb ccc', 'ddd']}
 
-	b = DummyArgs( body_search_strs = s, match_ANY_body = True ) 
-	assert entry_matchesQ( entry_book, b )
+	a = DummyArgs( title_search_strs = ['xxx'], body_search_strs = ['ddd'] )
+	assert not entry_matchesQ( e, a )
 
-
-def test_match_title_OR_body(entry_apples, entry_book):
-	a = DummyArgs( title_search_strs = ['foo'], body_search_strs = ['chapter'] )
-	assert not entry_matchesQ( entry_book, a )
-
-	b = DummyArgs( title_search_strs = ['foo'], body_search_strs = ['chapter'], match_title_OR_body = True )
-	assert entry_matchesQ(entry_book, b)
+	a.match_title_OR_body = True
+	assert entry_matchesQ(e, a)
 
